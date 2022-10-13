d7bf89655735d916a77fd8e992385c623dd29074
[bhopendra] <[bhopendrasahu2899@gmail.com]>
2022-10-13 13:13:47 +0530

    Encapsulation code write

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -3,31 +3,23 @@ package com.driver;
 public class Main {
 public static void main(String[] args){
     Product p=new Product();
-    System.out.println(p.product(3,4));
-    System.out.println(p.product(3,4,5));
-    System.out.println(p.product(3.1,4.12));
+    int ans=p.product(3,4);
+
+            int ans1=p.product(3,4,5);
+                    double ans2=p.product(3.1,4.12)
+    System.out.println(ans+ " "+ ans1+" "+ans2);
+    //System.out.println();
+   // System.out.println();
 }
-}
-class Product{
-   int x;
-   int y;
-   int z;
-   double a;
-   double b;
-   int product(int x,int y){
-       this.x=x;
-       this.y=y;
-       return x*y;
-   }
-    int product(int x,int y,int z){
-        this.x=x;
-        this.y=y;
-        this.z=z;
+public static class Product{
+    public int product(int x,int y){
+        return x*y;
+    }
+    public int product(int x,int y,int z){
         return x*y*z;
     }
-    double product(double x,double y){
-       a=x;
-       b=y;
-       return a*b;
+    public double product(double x,double y){
+        return x*y;
     }
+}
 }
